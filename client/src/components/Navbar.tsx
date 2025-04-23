7317d1b147d7c6998647e616fb2837c7f28239fc
Aman kumar yadav <amaninsane139@gmail.com>
2025-04-24 02:05:30 +0530

    Nav Bar UI

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,7 +1,13 @@
 export default function Navbar(){
     return(
-        <div>
-            mastikhor
+        <div className="flex relative justify-center">
+            <div className="w-[150vh] mt-4 flex items-center justify-around font-bold text-2xl bg-black rounded-4xl h-[60px] text-white font-mullish ">
+                <div className="rounded-4xl pt-1.5 pb-1.5 pr-19.5 pl-19.5 hover:bg-white hover:text-black transition cursor-pointer duratio9.500">Hello</div>
+                <div className="rounded-4xl pt-1.5 pb-1.5 pr-19.5 pl-19.5 hover:bg-white hover:text-black transition cursor-pointer duratio9.500">How</div>
+                <div className="rounded-4xl pt-1.5 pb-1.5 pr-19.5 pl-19.5 hover:bg-white hover:text-black transition cursor-pointer duratio9.500">Are</div>
+                <div className="rounded-4xl pt-1.5 pb-1.5 pr-19.5 pl-19.5 hover:bg-white hover:text-black transition cursor-pointer duratio9.500">You</div>
+            </div>
+           
         </div>
     )
 }
